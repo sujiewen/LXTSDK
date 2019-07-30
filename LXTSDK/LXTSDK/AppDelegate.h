2f58c027e22a41bfbc3ec712592274f531ffdf30
sjw <sujiewen@ideal-link.com>
2019-07-30 15:32:31 +0800

    完善

---
+++ LXTSDK/LXTSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXTSDK
+//
+//  Created by sjw on 2019/7/30.
+//  Copyright © 2019 sjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXTSDK/LXTSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXTSDK
+//
+//  Created by sjw on 2019/7/30.
+//  Copyright © 2019 sjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
